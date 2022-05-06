968c17f5a35a203e72b34f7f83f8d0bac5e0ea4f
Sergey Biryukov <nightofferocity@gmail.com>
2022-05-06 15:57:09 +0500

    complete first task

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+import kotlin.math.roundToInt
+
+fun main() {
+    val amount = 10.0
+    val transferMinimum = 35
+    if (amount < transferMinimum) {
+        println("Минимумальная сумма перевода ${transferMinimum} рублей")
+    } else {
+        val commission = calculateCommission(amount)
+        val penny = getPenny(commission)
+        val money: String = if (penny >= 100) "${penny / 100} р. ${penny % 100} коп." else "$penny коп."
+            println("Комиссия составляет: $money")
+    }
+
+}
+
+fun calculateCommission(amount: Double): Double {
+    val percent = 0.0075
+    return percent * amount
+}
+
+fun getPenny(amount: Double): Int {
+    return (amount * 100).roundToInt()
+}
